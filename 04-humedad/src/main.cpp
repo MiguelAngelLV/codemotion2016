ac70c97cb611d7f1c14101e45745e0403e149662
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-11-19 12:54:09 +0100

    Updated 04-humedad

--- 04-humedad/src/main.cpp
+++ 04-humedad/src/main.cpp
@@ -1,25 +1,36 @@
 #include <Arduino.h>
 #include "DHT.h"
+
+//GPIO sensor DHT
 #define DHT_GPIO D3
 
+//Sensor de Humedad
 DHT dht(DHT_GPIO, DHT11);
 
+//----------------------------------------------
+
 void setup() {
+  //Iniciamos el Serial
   Serial.begin(9600);
+
+  //Iniciamos el sensor
   dht.begin();
 }
 
+//----------------------------------------------
 
 void loop() {
-
+  //Esperamos 1 minuto entre actualización
   delay(2*1000);
 
+  //Leemos los valores del sensor
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
-  if (isnan(humidity) || isnan(temperature))
+  //El DHT a veces no lee bien y da errores
+  if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Error de lectura");
-  else {
+  } else {
     Serial.print("Humedad: ");
     Serial.println(humidity, 2);
     Serial.print("Temperatura: ");
