bc539a527960f664cde1a70ce4b21139f6552be1
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-11-19 12:45:56 +0100

    Updated semaforo

--- 02-semaforo/src/main.cpp
+++ 02-semaforo/src/main.cpp
@@ -1,6 +1,43 @@
-
 #include <Arduino.h>
 
+//GPIO leds
 #define RED D1
 #define YELLOW D2
 #define GREEN D3
+
+//----------------------------------------------
+
+void setup() {
+  //Los pines de los leds
+  //los configuramos como salida
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+
+  //Apagamos los leds
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+}
+
+//----------------------------------------------
+
+void loop() {
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+
+  delay(1000);
+
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(GREEN, LOW);
+
+  delay(1000);
+
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, HIGH);
+
+  delay(1000);
+}
