32e94b1cfce79323cf16a8887718a9fbda700fb6
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-11-19 11:16:16 +0100

    Added comments

--- smart-choza-coche/src/main.cpp
+++ smart-choza-coche/src/main.cpp
@@ -1,32 +1,29 @@
 #include <Arduino.h>
-#include "DHT.h"
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-
+//Datos conexión WiFi
 #define WLAN_SSID       "Codemotion_2016"
 #define WLAN_PASS       "codemotion2016"
 
-
+//Datos conexión con Adafruit IO
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    ""
 #define AIO_KEY         ""
 #define AIO_FEED        AIO_USERNAME "/feeds/"
 
-
 //GPIO sensor de distancia
 #define TRIGGER D1
 #define ECHO    D2
 
-
-
+//GPIO leds
 #define RED D6
 #define YELLOW D7
 #define GREEN  D8
 
-
+//Umbrales para estimar la distancia
 #define FAR    60
 #define MEDIUM 30
 #define NEAR   15
@@ -37,24 +34,20 @@ WiFiClient client;
 //Conexión a Adafruit
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
 
-
-//Feeds para publicar información
+//Feed para publicar información
 Adafruit_MQTT_Publish parkedFeed = Adafruit_MQTT_Publish(&mqtt, AIO_FEED "parked");
 
 //Feed para leer información
 Adafruit_MQTT_Subscribe enabledFeed = Adafruit_MQTT_Subscribe(&mqtt, AIO_FEED "enabled");
 
-
 bool enabled;
 bool parked;
 
-
-void connect();
-int time2cm(int time);
+void connectWiFi();
 void connectAdafruit();
+int time2cm(int time);
 
 void setup() {
-
   //El trigger lanzará el pulso,
   //lo configuramos como salida
   pinMode(TRIGGER, OUTPUT);
@@ -64,12 +57,13 @@ void setup() {
   //lo configuramos como entrada
   pinMode(ECHO, INPUT);
 
-
-
+  //Los pines de los leds
+  //los configuramos como salida
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(GREEN, OUTPUT);
 
+  //Apagamos los leds
   digitalWrite(RED, LOW);
   digitalWrite(YELLOW, LOW);
   digitalWrite(GREEN, LOW);
@@ -77,21 +71,22 @@ void setup() {
   enabled = true;
   parked  = false;
 
-
   Serial.begin(9600);
   delay(200);
-  connect();
+
+  // Conectamos la WiFI y con Adafruit
+  connectWiFi();
   connectAdafruit();
 
+  //Nos sucribimos al topic: enabled
   mqtt.subscribe(&enabledFeed);
 
+  //Encendemos el led que lleva la placa integrado
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, enabled);
-
 }
 
 void loop() {
-
   Adafruit_MQTT_Subscribe * subscription = mqtt.readSubscription(200);
 
   if (subscription == &enabledFeed) {
@@ -100,13 +95,11 @@ void loop() {
     else
         enabled = false;
 
-    digitalWrite(LED_BUILTIN, enabled);
+    digitalWrite(BUILTIN_LED, enabled);
   }
-
 
   if (!enabled)
     return;
-
 
   //Esperamos 4 microsegundos para asegurarnos de que no hay
   //ondas residuales que hagan interferencias
@@ -118,6 +111,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIGGER, LOW);
 
+  //Calculamos la distancia
   int duration = pulseIn(ECHO, HIGH);
   int distance = time2cm(duration);
 
@@ -127,9 +121,9 @@ void loop() {
 
   if (distance < FAR) {
     Serial.printf("Duración: %d. Distancia: %d\n", duration, distance);
-  }
-  else
+  } else {
     Serial.println("Fuera de rango");
+  }
 
   digitalWrite(GREEN, distance < FAR);
   digitalWrite(YELLOW, distance < MEDIUM);
@@ -144,12 +138,9 @@ void loop() {
     parkedFeed.publish("Desaparcando");
     parked = false;
   }
-
-
 }
 
-
-void connect() {
+void connectWiFi() {
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
      delay(500);
@@ -161,9 +152,7 @@ void connect() {
   Serial.println(WiFi.localIP());
 }
 
-
 void connectAdafruit() {
-
   if (mqtt.connected())
     return;
 
@@ -173,9 +162,7 @@ void connectAdafruit() {
        mqtt.disconnect();
        delay(5000);
   }
-
 }
-
 
 int time2cm(int time) {
   return (int) round(time * 0.0171232877);
